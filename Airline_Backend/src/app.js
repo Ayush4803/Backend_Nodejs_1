78068f0766a2fbc6bd488ba57b865b7787658c38
Ayush <ayushkumar55526@gmail.com>
2025-09-21 12:43:19 +0530

    created the city repository

--- Airline_Backend/src/app.js
+++ Airline_Backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const bodyparser = require('body-parser')
 const dotenv = require('dotenv')
-const CityRepository=require('./repository/city-repository')
+// const CityRepository=require('./repository/city-repository')
 dotenv.config()
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -12,23 +12,20 @@ app.listen(PORT,  async() => {
     console.log(`Succesfully started at PORT:${PORT}`)
 
     //creating cities
-    const repo=new CityRepository()
-   await repo.createCity({name:"New Delhi"})
-   await repo.createCity({name:"Jaiselmair"})
-   await repo.createCity({name:"Kochi"})
-   await repo.createCity({name:"Jaipur"})
-   await repo.createCity({name:"Ahemdabad"})
+    // const repo=new CityRepository()
+//    await repo.createCity({name:"New Delhi"})
+//    await repo.createCity({name:"Jaiselmair"})
+//    await repo.createCity({name:"Kochi"})
+//    await repo.createCity({name:"Jaipur"})
+//    await repo.createCity({name:"Ahemdabad"})
 
     //deleteing cities
 
-//    await repo.deleteCity(8)
-//    await repo.deleteCity(9)
-//    await repo.deleteCity(10)
-//    await repo.deleteCity(11)
-//    await repo.deleteCity(12)
-//    await repo.deleteCity(13)
-//    await repo.deleteCity(26)
-//    await repo.deleteCity(31)
-//    await repo.deleteCity(10)
+//    await repo.deleteCity(47)
+//    await repo.deleteCity(48)
+//    await repo.deleteCity(49)
+//    await repo.deleteCity(50)
+//    await repo.deleteCity(51)
+ 
 
 })
