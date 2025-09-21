3f6b4e43e1c5892c3eb7f125c53bef093563440e
Ayush <ayushkumar55526@gmail.com>
2025-09-21 14:17:16 +0530

    Added the controllers

---
+++ Airline_Backend/src/controllers/city-controller.js
@@ -0,0 +1,91 @@
+const { CityService } = require('../sercices/index');
+
+const cityService = new CityService();
+
+const create = async (req, res) => {
+    try {
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data: city,
+            success: true,
+            message: "Succesfully created a city",
+            error: {}
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to create the city",
+            err: error
+        })
+    }
+}
+
+
+const destroy = async (req, res) => {
+    try {
+      const response = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Succesfully deleted a city",
+            error: {}
+        })
+    } catch (error) {
+  console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to delete the city",
+            err: error
+        })
+    }
+}
+
+const get = async (req, res) => {
+    try {
+     const response = await cityService.getCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Succesfully fetched a city",
+            error: {}
+        })
+    } catch (error) {
+ console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to patched the city",
+            err: error
+        })
+    }
+}
+
+const update = async (req, res) => {
+    try {
+  const response = await cityService.updateCity(req.params.id,req.body);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Succesfully updated a city",
+            error: {}
+        })
+    } catch (error) {
+console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to update the city",
+            err: error
+        })
+    }
+}
+
+module.exports={
+    create,
+    destroy,
+    get,
+    update
+}
--- Airline_Backend/src/services/city-service.js
+++ Airline_Backend/src/services/city-service.js
@@ -1,54 +1,54 @@
-const {CityRepository}= require('../repository/index')
+const { CityRepository } = require('../repository/index')
 
 
-class CityService{
+class CityService {
 
-constructor(){
-    this.cityRepository=new CityRepository();
-}
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
 
- async createCity(data){
-  try {
-    const city=await this.cityRepository.createCity(data)
-    return city;
-  }
-   catch (error) {
-    console.log("Something went Wrong in service layer");
-    throw{error};
-  }
- }
-
-
-async deleteCity(cityId){
-    try {
-    const response= await this.cityRepository.deleteCity(cityId) 
-    return response;
-  } catch (error) {
-    console.log("Something went Wrong in service layer");
-    throw{error};
-  }
-}
-
-async updateCity(cityId,data){
-    try {
-    const city = await this.cityRepository.updateCity(cityId,data)
-    return city;
-  } catch (error) {
-    console.log("Something went Wrong in service layer");
-    throw{error};
-  }
-}
+    async createCity(data) {
+        try {
+            const city = await this.cityRepository.createCity(data)
+            return city;
+        }
+        catch (error) {
+            console.log("Something went Wrong in service layer");
+            throw { error };
+        }
+    }
 
 
-async getCity(cityId){
-    try {
-    const city= await this.cityRepository.getCity(cityId)
-    return city;
-  } catch (error) {
-    console.log("Something went Wrong in service layer");
-    throw{error};
-  }
-}
+    async deleteCity(cityId) {
+        try {
+            const response = await this.cityRepository.deleteCity(cityId)
+            return response;
+        } catch (error) {
+            console.log("Something went Wrong in service layer");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityRepository.updateCity(cityId, data)
+            return city;
+        } catch (error) {
+            console.log("Something went Wrong in service layer");
+            throw { error };
+        }
+    }
+
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRepository.getCity(cityId)
+            return city;
+        } catch (error) {
+            console.log("Something went Wrong in service layer");
+            throw { error };
+        }
+    }
 
 }
 
