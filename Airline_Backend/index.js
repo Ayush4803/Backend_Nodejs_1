5714d9b488b18765b13cd1755c72c26ef5d95155
Ayush <ayushkumar55526@gmail.com>
2025-09-23 12:19:42 +0530

    Added Airplane table/model & Seed folder

---
+++ Airline_Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const bodyparser = require('body-parser')
+const dotenv = require('dotenv')
+const ApiRoutes= require('./src/routes/index')
+
+dotenv.config()
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use('/api',ApiRoutes)
+const PORT = process.env.PORT
+app.listen(PORT,  async() => {
+    console.log(`Succesfully started at PORT:${PORT}`)
+})
---
+++ Airline_Backend/src/models/airplane.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Airplane extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Airplane.init({
+ type: {modelNumber: DataTypes.STRING,
+  allowNull:false
+ },
+    type:{capacity: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:150
+    }
+  }, {
+    sequelize,
+    modelName: 'Airplane',
+  });
+  return Airplane;
+};
