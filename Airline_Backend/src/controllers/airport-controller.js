5157f634624b2543105574e58b85dcc9349b4195
Ayush Kumar <140965613+Ayush4803@users.noreply.github.com>
2025-10-04 19:05:08 +0530

    Update airport-controller.js

--- Airline_Backend/src/controllers/airport-controller.js
+++ Airline_Backend/src/controllers/airport-controller.js
@@ -5,7 +5,7 @@ async function create(req, res) {
     try {
         const response = await airportRepository.create(req.body);
 
-        return res.status(201).json({
+        return res.status(200).json({
             success: true,
             message: "Airport created successfully",
             data: response,
